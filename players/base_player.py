d28c4cf651109467ca4052a49a7ee0ed50850a10
Nguyen Cong Dat <traceformula2000@gmail.com>
2021-05-10 11:14:49 +0700

    Development | Environment | Implement base player

---
+++ players/base_player.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+import sys
+sys.path.append('../')
+from environment.env_config import ACTIONS
+
+
+class BasePlayer(ABC):
+    def __init__(self):
+        super(BasePlayer, self).__init__()
+
+    @abstractmethod
+    def play(self, state):
+        """
+            Try to make an action based on the observation state
+        :param
+            state: current game state, in form of numpy.array of gray image
+        :return:
+            one of 3 action:
+                stay - doing nothing
+                jump
+                bow
+        """
+        return ACTIONS['stay']
